7e4273c0060ef29c35cf65e6eac6dbfe00006ff2
Lee Minhyeung <71058886+Leecarry16@users.noreply.github.com>
2022-05-24 00:55:36 +0900

    엑셀 파일과 비교하여 파일을 폴더에 넣어주는 프로그램
    거의 완벽에 가까움

--- calssifier.py
+++ calssifier.py
@@ -11,49 +11,31 @@ import sys
 import shutil
 import re
 
-dataframeXlsx = pd.read_excel("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/이미지 분류 진행과정.xlsx", sheet_name="Sheet1")
-
-'''print(dataframeXlsx)
-print()'''
-
-## From https://swlock.blogspot.com/2021/10/python-count-files.html
-def get_files_count(folder_path):
-	dirListing = os.listdir(folder_path)
-	return len(dirListing)
-	
-#shutil.move("C:/Users/user/Desktop\School/AI_Build_Project_test/Term Projecct/Program/rename/20220510155254,.JPG", "C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/1620")
-
-'''print(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/0105"))
-print(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/0610"))
-print(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/1115"))
-print(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/1620"))
-print(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/2125"))
-print(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/2630"))
-print(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/3135"))
-print(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/3640"))
-print(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/4145"))
-print(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/4650"))
-print(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/5155"))
-print(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/5660"))
-print(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/6165"))
-print(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/6670"))
-print(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/7175"))
-print(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/7680"))
-print(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/8185"))
-print(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/8690"))
-print(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/9195"))
-print(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/9600"))'''
-#print(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/nan"))
-
-
 im_path = "C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/rename"
 file_list = os.listdir(im_path)
 
-pm_exc = pd.read_excel("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10_20220522.xlsx")
+pm_dir = ["0105", "0610", "1115", "1620", "2125", "2630", "3135", "3640", "4145", "4650", "5155", "5660", "6165", "6670", "7175", "7680", "8185", "8690", "9195", "9600", "nan", "error"]
 
-print(type(pm_exc.iat[6,2]))
-print(type(file_list))
-print(float('nan'))
+
+pm_path = "C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/{}"
+
+#print(dataframeXlsx)
+
+def get_files_count(folder_path):
+	dirListing = os.listdir(folder_path)
+	return len(dirListing)
+
+
+sum_before = 0
+for pm_dir_num in range(21):
+	print(pm_dir[pm_dir_num], get_files_count(pm_path.format(pm_dir[pm_dir_num])))
+	sum_before = sum_before + int(get_files_count(pm_path.format(pm_dir[pm_dir_num])))
+
+print("분류 전 폴더 내 파일 개수: ", sum_before)
+
+
+
+pm_exc = pd.read_excel("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10_20220522.xlsx")
 
 num = 0
 test_list = []
@@ -70,90 +52,36 @@ for file in file_list:
 			for col_num in range(1, 25):
 				if name_h in re.sub(r'[^0-9]', '', str(pm_exc.iat[1, col_num])):
 					pm_hour = pm_exc.iat[2, col_num]
-					test_list.append(pm_exc.iat[row_num, col_num])
-					
-					if 1 <= pm_exc.iat[row_num, col_num] <= 5:
-						shutil.move("C:/Users/user/Desktop\School/AI_Build_Project_test/Term Projecct/Program/rename/{}".format(file_list[num]), "C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/0105")
-					elif 6 <= pm_exc.iat[row_num, col_num] <= 10:
-						shutil.move("C:/Users/user/Desktop\School/AI_Build_Project_test/Term Projecct/Program/rename/{}".format(file_list[num]), "C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/0610")
-					elif 11 <= pm_exc.iat[row_num, col_num] <= 15:
-						shutil.move("C:/Users/user/Desktop\School/AI_Build_Project_test/Term Projecct/Program/rename/{}".format(file_list[num]), "C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/1115")
-					elif 16 <= pm_exc.iat[row_num, col_num] <= 20:
-						shutil.move("C:/Users/user/Desktop\School/AI_Build_Project_test/Term Projecct/Program/rename/{}".format(file_list[num]), "C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/1620")
-					elif 21 <= pm_exc.iat[row_num, col_num] <= 25:
-						shutil.move("C:/Users/user/Desktop\School/AI_Build_Project_test/Term Projecct/Program/rename/{}".format(file_list[num]), "C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/2125")
-					elif 26 <= pm_exc.iat[row_num, col_num] <= 30:
-						shutil.move("C:/Users/user/Desktop\School/AI_Build_Project_test/Term Projecct/Program/rename/{}".format(file_list[num]), "C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/2630")
-					elif 31 <= pm_exc.iat[row_num, col_num] <= 35:
-						shutil.move("C:/Users/user/Desktop\School/AI_Build_Project_test/Term Projecct/Program/rename/{}".format(file_list[num]), "C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/3135")
-					elif 36 <= pm_exc.iat[row_num, col_num] <= 40:
-						shutil.move("C:/Users/user/Desktop\School/AI_Build_Project_test/Term Projecct/Program/rename/{}".format(file_list[num]), "C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/3640")
-					elif 41 <= pm_exc.iat[row_num, col_num] <= 45:
-						shutil.move("C:/Users/user/Desktop\School/AI_Build_Project_test/Term Projecct/Program/rename/{}".format(file_list[num]), "C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/4145")
-					elif 46 <= pm_exc.iat[row_num, col_num] <= 50:
-						shutil.move("C:/Users/user/Desktop\School/AI_Build_Project_test/Term Projecct/Program/rename/{}".format(file_list[num]), "C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/4650")
-					elif 51 <= pm_exc.iat[row_num, col_num] <= 55:
-						shutil.move("C:/Users/user/Desktop\School/AI_Build_Project_test/Term Projecct/Program/rename/{}".format(file_list[num]), "C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/5155")
-					elif 56 <= pm_exc.iat[row_num, col_num] <= 60:
-						shutil.move("C:/Users/user/Desktop\School/AI_Build_Project_test/Term Projecct/Program/rename/{}".format(file_list[num]), "C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/5660")
-					elif 61 <= pm_exc.iat[row_num, col_num] <= 65:
-						shutil.move("C:/Users/user/Desktop\School/AI_Build_Project_test/Term Projecct/Program/rename/{}".format(file_list[num]), "C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/6165")
-					elif 66 <= pm_exc.iat[row_num, col_num] <= 70:
-						shutil.move("C:/Users/user/Desktop\School/AI_Build_Project_test/Term Projecct/Program/rename/{}".format(file_list[num]), "C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/6670")
-					elif 71 <= pm_exc.iat[row_num, col_num] <= 75:
-						shutil.move("C:/Users/user/Desktop\School/AI_Build_Project_test/Term Projecct/Program/rename/{}".format(file_list[num]), "C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/7175")
-					elif 76 <= pm_exc.iat[row_num, col_num] <= 80:
-						shutil.move("C:/Users/user/Desktop\School/AI_Build_Project_test/Term Projecct/Program/rename/{}".format(file_list[num]), "C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/7680")
-					elif 81 <= pm_exc.iat[row_num, col_num] <= 85:
-						shutil.move("C:/Users/user/Desktop\School/AI_Build_Project_test/Term Projecct/Program/rename/{}".format(file_list[num]), "C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/8185")
-					elif 86 <= pm_exc.iat[row_num, col_num] <= 90:
-						shutil.move("C:/Users/user/Desktop\School/AI_Build_Project_test/Term Projecct/Program/rename/{}".format(file_list[num]), "C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/8690")
-					elif 91 <= pm_exc.iat[row_num, col_num] <= 95:
-						shutil.move("C:/Users/user/Desktop\School/AI_Build_Project_test/Term Projecct/Program/rename/{}".format(file_list[num]), "C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/9195")
-					elif 96 <= pm_exc.iat[row_num, col_num] <= 100:
-						shutil.move("C:/Users/user/Desktop\School/AI_Build_Project_test/Term Projecct/Program/rename/{}".format(file_list[num]), "C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/9600")
-					else:
-						shutil.move("C:/Users/user/Desktop\School/AI_Build_Project_test/Term Projecct/Program/rename/{}".format(file_list[num]), "C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/nan")
-						
-					num = num + 1
+					pm_cell = pm_exc.iat[row_num, col_num]
+					test_list.append(pm_cell)
+					try:
+						for pm_cls in range(20):
+							if (1 + (pm_cls*5)) <= pm_cell <= (5 + (pm_cls * 5)):
+								shutil.move("C:/Users/user/Desktop\School/AI_Build_Project_test/Term Projecct/Program/rename/{}".format(file_list[file]), pm_path.format(pm_dir[pm_cls]))
+							else:
+								shutil.move("C:/Users/user/Desktop\School/AI_Build_Project_test/Term Projecct/Program/rename/{}".format(file_list[file]), pm_path.format("nan"))
+					except shutil.Error:
+						shutil.move("C:/Users/user/Desktop\School/AI_Build_Project_test/Term Projecct/Program/rename/{}".format(file_list[file]), pm_path.format("error"))
 
-print(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/nan"))
-print(test_list)
-print(len(test_list))
-
-
+print("분류한 파일 개수: ", num)
 
 #print(pm_exc)
 
-
 rb = load_workbook("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/이미지 분류 진행과정.xlsx")
-
 ws = rb['Sheet1']
 wb = openpyxl.load_workbook("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/이미지 분류 진행과정.xlsx")
 
 Sheet1 = wb.active 
-Sheet1['B2'] = int(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/0105"))
-Sheet1['B3'] = int(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/0610"))
-Sheet1['B4'] = int(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/1115")) 
-Sheet1['B5'] = int(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/1620"))
-Sheet1['B6'] = int(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/2125"))
-Sheet1['B7'] = int(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/2630"))
-Sheet1['B8'] = int(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/3135"))
-Sheet1['B9'] = int(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/3640"))
-Sheet1['B10'] = int(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/4145"))
-Sheet1['B11'] = int(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/4650"))
-Sheet1['B12'] = int(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/5155"))
-Sheet1['B13'] = int(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/5660"))
-Sheet1['B14'] = int(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/6165"))
-Sheet1['B15'] = int(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/6670"))
-Sheet1['B16'] = int(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/7175"))
-Sheet1['B17'] = int(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/7680"))
-Sheet1['B18'] = int(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/8185"))
-Sheet1['B19'] = int(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/9195"))
-Sheet1['B21'] = int(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/9600"))
-Sheet1['B22'] = int(get_files_count("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/PM10 Class/nan"))
+
+sum_after = 0
+
+for pm_dir_num in range(22):
+	Sheet1['B{}'.format(pm_dir_num + 2)] = int(get_files_count(pm_path.format(pm_dir[pm_dir_num])))
+	sum_after = sum_after + int(get_files_count(pm_path.format(pm_dir[pm_dir_num])))
+
+Sheet1['B24'] = sum_after
+print("분류 후 폴더 내 파일 개수: ",sum_after)
 
 wb.save("C:/Users/user/Desktop/School/AI_Build_Project_test/Term Projecct/Program/이미지 분류 진행과정.xlsx")
-
 
 
